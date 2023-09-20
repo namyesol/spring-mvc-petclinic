dea9fc92d2b81a6cfecd01dda1b3f4fa8012ecaf
namyesol <studhaza@naver.com>
2023-09-20 16:39:37 +0900

    [#5]SimpleMappingExceptionResolver 설정

---
+++ src/main/java/com/namyesol/petclinic/controller/CrashController.java
@@ -0,0 +1,17 @@
+package com.namyesol.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller used to showcase what happens when an exception is thrown 
+ */
+@Controller
+public class CrashController {
+	
+	@GetMapping("/oups")
+	public String triggerException() {
+		throw new RuntimeException("Expected: controller used to showcase what " +
+	            "happens when an exception is thrown");
+	}
+}
